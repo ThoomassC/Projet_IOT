69c8c076c28123639ba3a03cee72b655e38953c8
Thomas.C <thomas.caron@viacesi.fr>
2023-02-08 15:21:53 +0100

    API

--- CodeAPI/Repository.py
+++ CodeAPI/Repository.py
@@ -2,10 +2,11 @@
 import os
 import sqlite3
 from sqlite3 import Error
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+# Création du fichier de BDD
 db_file = r"Projet_IOT.db"
 
 def init_database(connection):
@@ -30,40 +31,19 @@ def create_connection():
                 init_database(connection)
             return connection
 
-
 conn = create_connection()
 
+# Pour récupérer le json de l'ESP
 @app.route("/api/data", methods=['POST'])
 def insertDatas():
-    json = request.json
+    json : dict = request.json
     cursor = conn.cursor()
     cursor.execute(
         f"""INSERT INTO Meteo (heure, jour, pression, temperature, humidite) 
-        VALUES ({json.get('heure')}, {json.get('jour')}, {json.get('pression')}, {json.get('temperature')}, {json.get('humidite')});""")
+        VALUES (\'{json.get('heure')}\', {json.get('jour')}, {json.get('pression')}, {json.get('temperature')}, {json.get('humidite')});""")
     conn.commit()
     return jsonify(True)
 
-@app.route('/get_Datas/')
-def get_datas():
-    cursor = conn.cursor()
-    cursor.execute("SELECT id, heure, jour, pression, temperature, humidite FROM Meteo;")
-    queries = cursor.fetchall()
-    result = []
-    for query in queries:
-        result.append({
-            'id': query[0],
-            'date_prise': query[1],
-            'pression': query[3],
-            'temperature': query[4],
-            'humidite': query[5],
-        })
-    return render_template('TemplateProjet_IOT.html', templateData=jsonify(result))
-
-
-@app.route('/getDatas/<int:temperature>/', methods=['GET'])
-def getDatas(temperature):
-    # récupérer les variables dans la route puis paramètre et faire un SELECT
-    return True
-
+# pour lancer le server
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
